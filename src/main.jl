449ac44b70ba4b523e1d7413566fb7c8df203ca2
RyotaBannai <ryotala0528@gmail.com>
2021-09-21 00:12:45 +0900

    add: Inner constructor methods.

--- src/main.jl
+++ src/main.jl
@@ -2,7 +2,7 @@ using Dates
 using Lazy
 using PartialFunctions
 
-#= 
+#=
 - Julia Types | <https://docs.julialang.org/en/v1/manual/types/>
 - What is difference between Type{T} and T | <https://discourse.julialang.org/t/what-is-difference-between-type-t-and-t/19325/57>
 - UnionAll | <https://docs.julialang.org/en/v1/devdocs/types/#UnionAll-types>
@@ -85,7 +85,8 @@ add_one4() = inp .|> sqrt .|> (x -> x * 2)  # 初めに sqrt を適用
 add_one5() = @. inp |> sqrt |> (*)$2        # 初めに sqrt を適用
 
 # broadcasing := それぞれの element に対し、index の対応する関数をそれぞれ適用
-ziplike() = ["a", "list", "of", "strings"] .|> [uppercase, reverse, titlecase, length]
+ziplike() =
+    ["a", "list", "of", "strings"] .|> [uppercase, reverse, titlecase, length]
 
 f(x, y) = 3x + 4y;
 A = [1.0, 2.0, 3.0];
@@ -94,13 +95,13 @@ f.(pi, A) # like `zipLongest`
 f.(A, B)  # like simple `zip`
 
 #=
-These are the same: 
+These are the same:
 
-1. sin.(cos.(X)) 
+1. sin.(cos.(X))
 2. broadcast(x -> sin(cos(x)), X)
 3. [sin(cos(x)) for x in X]
 
-Technically, the `fusion` stops as soon as a "non-dot" function call is encountered; 
+Technically, the `fusion` stops as soon as a "non-dot" function call is encountered;
 for example, in `sin.(sort(cos.(X)))` the `sin` and `cos` loops cannot be merged because of the intervening `sort` function.
 =#
 
@@ -132,10 +133,11 @@ struct MyRecord
     vec::Vector{UInt}
 end
 
+# フィールドが指す address `は変更できないが、mutable` フィールド自体は変更可能
 r = MyRecord(1, 2, [1, 2, 3])
-push!(r.vec, 4) # フィールドが指す address `は変更できないが、mutable` フィールド自体は変更可能
+push!(r.vec, 4)
 
-# you can change struct to `mutable` one 
+# you can change struct to `mutable` one
 struct mutable
     MMyRecord::Any
     first::UInt
@@ -182,9 +184,9 @@ const T2 = Array{Array{T,1},1} where {T}
 #= |
 <https://docs.julialang.org/en/v1/manual/types/#UnionAll-Types>
 
-Type `T1` defines a 1-dimensional array of 1-dimensional arrays; each of the inner arrays consists of objects of the same type, but this type may vary from one inner array to the next. 
+Type `T1` defines a 1-dimensional array of 1-dimensional arrays; each of the inner arrays consists of objects of the same type, but this type may vary from one inner array to the next.
 
-On the other hand, type `T2` defines a 1-dimensional array of 1-dimensional arrays all of whose inner arrays must have the same type. 
+On the other hand, type `T2` defines a 1-dimensional array of 1-dimensional arrays all of whose inner arrays must have the same type.
 
 Note that `T2` is an abstract type, e.g., `Array{Array{Int,1},1} <: T2`, whereas `T1` is a concrete type. As a consequence, `T1` can be constructed with a zero-argument constructor `a=T1()` but `T2` cannot.
 =#
@@ -194,7 +196,7 @@ struct WrapType{T}
 end
 
 #=
-WrapType(Float64) # default constructor, note DataType 
+WrapType(Float64) # default constructor, note DataType
 WrapType{DataType}(Float64)
 """
 WrapType(::Type{T}) where {T} = WrapType{Type{T}}(T)
@@ -230,7 +232,7 @@ a = Polar(3,4.0)
 # shows `:((3.0 * exp(4.0im)) ^ 2)` instead of `3.0 * exp(4.0im) ^ 2`
 =#
 
-# passing context 
+# passing context
 function Base.show(io::IO, z::Polar)
     get(io, :compact, false) ? print(io, z.r, "e", z.Θ, "im") :
     print(io, z.r, " * exp(", z.Θ, "im)")
@@ -251,7 +253,7 @@ same_type(x, y) = false
 testm(x::Int) = "definition for Int"
 testm(x::Type{Int}) = "definition for Type{Int}"
 
-# DataType <: Type{T} <: Any 
+# DataType <: Type{T} <: Any
 # typeof(AbstractArray) = AbstractArray, supertype(AbstractArray) = UnionAll <: Type{T}
 # Array <: DenseArray <: AbstractArray -- all type are UnionAll
 
@@ -266,3 +268,25 @@ input = Array{UInt}(undef, 2)
 copy_with_eltype(input, Eltype) = copyto!(similar(input, Eltype), input)
 
 # `f(x::Int...) = x` is a shorthand for `f(x::Vararg{Int}) = x`
+
+struct Foo
+    bar::Any
+    baz::Any
+end
+
+#=
+re-define constructor (called `outer constructor methods`)
+however, `outer constructor methods` fails to address these two problems:
+1. enforcing `invariants`(:= are kind of rules, guards)
+2. allowing construction of self-referential objects. For these problems
+=#
+
+Foo(x) = Foo(x, x + 1)
+Foo() = Foo(0)
+
+# Using inner constructor methods
+struct OrderedPair
+    fst::Real
+    snd::Real
+    OrderedPair(fst, snd) = fst > snd ? error("out of order") : new(fst, snd)
+end
